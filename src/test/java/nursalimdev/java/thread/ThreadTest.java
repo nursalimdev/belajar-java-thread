d37a3d4fe0a3206c624845455b47b6955b4d1c5e
nursalimdev <nursalimdev123@gmail.com>
2023-01-31 20:11:49 +0700

    Update ThreadTest.java

--- src/test/java/nursalimdev/java/thread/ThreadTest.java
+++ src/test/java/nursalimdev/java/thread/ThreadTest.java
@@ -18,7 +18,73 @@ public class ThreadTest {
         thread.start();
 
         System.out.println("Program selesai");
+    }
 
+    @Test
+    void threadSleep(){
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from thread : " + Thread.currentThread().getName());
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+        System.out.println("Program selesai");
+
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void threadJoin() throws InterruptedException {
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from thread : " + Thread.currentThread().getName());
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+        System.out.println("Menunggu selesai");
+        thread.join();
+
+        System.out.println("Program selesai");
+    }
+
+    @Test
+    void threadInterrupt() throws InterruptedException {
+        Runnable runnable = () -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Runnable : " + i);
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+                    return;
+                }
+
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+        Thread.sleep(5000L);
+        thread.interrupt();
+        System.out.println("Menunggu selesai");
+        thread.join();
+
+        System.out.println("Program selesai");
     }
 
 }
